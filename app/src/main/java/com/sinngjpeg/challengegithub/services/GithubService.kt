2994b5d1931062e66bf3ea58591d2f72416bae17
sinng <ingridsilvatec@outlook.com>
2022-05-31 23:48:40 -0300

    implements get pull request list in github service

--- app/src/main/java/com/sinngjpeg/challengegithub/services/GithubService.kt
+++ app/src/main/java/com/sinngjpeg/challengegithub/services/GithubService.kt
@@ -1,11 +1,19 @@
 package com.sinngjpeg.challengegithub.services
 
 import com.sinngjpeg.challengegithub.model.GithubRepository
+import com.sinngjpeg.challengegithub.model.PullRequest
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
     @GET("/search/repositories?q=language:kotlin&sort=stars")
-    fun getRepositories(@Query("page") page: Int): Call<GithubRepository>
+    suspend fun getRepositories(@Query("page") page: Int): GithubRepository
+
+    @GET("/repos/{owner}/{repository}/pulls")
+    suspend fun getPullRequestList(
+        @Path("owner") owner: String,
+        @Path("repository") repository: String
+    ): List<PullRequest>
 }
